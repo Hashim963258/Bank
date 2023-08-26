d348e05eb6a5c73345da72842a43574fc4b05742
hashim <hashimbasha50@gmail.com>
2023-08-26 14:37:25 +0530

    My second commit

---
+++ BankAcccount/src/main/java/com/example/BankAcccount/BankAcccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.BankAcccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAcccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAcccountApplication.class, args);
+	}
+
+}
---
+++ BankAcccount/src/test/java/com/example/BankAcccount/BankAcccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankAcccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAcccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
